02a22d220ba676e8b5e44185484dda5432e691b2
Zaid Tapia <zaid.tapia@apdevs.com>
2025-10-14 23:51:30 -0600

    Seccion new collection

--- New_collection.php
+++ New_collection.php
@@ -1,37 +1,51 @@
-<?php 
-    $title = "KST";
-    $class="";
-    include 'includes/header.php'; 
-?>
-
-    <div class="container-all">
-        <div class="container-main">
-            <div class="container-landing"> 
-                <p style="font-family: 'Rubik';">Ahora Disponible</p>
-                <p>Nueva Colección</p>
+<div class="container my-5" id="seccion-nueva-coleccion">
+    <div class="row">
+        <div class="col-lg-4 col-md-6 col-sm-12 mb-4">
+            <div class="card h-100 shadow-sm border-0">
+                <a href="Sneakers.php" class="d-block text-decoration-none text-dark">
+                    <img src="images/new-colection1.webp" class="card-img-top img_container" alt="New Colection 1">
+                </a>
+                    <div class="card-body card-background">
+                        <h5 class="card-title fw-bold">Warrior Compression Tees</h5>
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item"><b>Material: </b>72% Polyamida, 21% Polyester, 7% Elastao.</li>
+                            <li class="list-group-item"><b>Peso de fabrica: </b>310 Gsm.</li>
+                            <li class="list-group-item"><b>Fit: </b> Super ajustado.</li>
+                        </ul>
+                    </div>
             </div>
-            <div class="container-images">
-                <div>   
-                    <img src="images/new-colection1.webp" alt="New Colection 1">
-                    <p><b>Material: </b>72% Polyamida, 21% Polyester, 7% Elastao.<br>
-                        <b>Peso de fabrica: </b>310 Gsm.<br>
-                        <b>Fit: </b> Super ajustado.
-                    </p>
+        </div>
+
+        <div class="col-lg-4 col-md-6 col-sm-12 mb-4">
+            <div class="card h-100 shadow-sm border-0">
+                <a href="Sneakers.php" class="d-block text-decoration-none text-dark">
+                    <img src="images/new-colection2.webp" class="card-img-top img_container" alt="New Colection 2">
+                </a>
+                <div class="card-body card-background">
+                    <h5 class="card-title fw-bold">Warrior Compression Tees</h5>
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item"><b>Material: </b>100% Algodon.<br></li>
+                        <li class="list-group-item"><b>Peso de fabrica: </b> 320 Gsm.<br></li>
+                        <li class="list-group-item"><b>FIT: </b> Relaxed</li>
+                    </ul>
                 </div>
-                <div>
-                    <img src="images/new-colection2.webp" alt="New Colection 2">
-                    <p><b>Material: </b>100% Algodon.<br>
-                        <b>Peso de fabrica: </b> 320 Gsm.<br>
-                        <b>FIT: </b> Relaxed
-                    </p>
+            </div>
+        </div>
+
+        <div class="col-lg-4 col-md-6 col-sm-12 mb-4">
+            <div class="card h-100 shadow-sm border-0">
+                <a href="URL_de_la_pagina_a_donde_quieres_ir.php" class="d-block text-decoration-none text-dark">
+                    <img src="images/new-colection3.webp" class="card-img-top img_container" alt="New Colection 3">
+                </a>
+                <div class="card-body card-background">
+                    <h5 class="card-title fw-bold">Warrior Compression Tees</h5>
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item"><b>Material: </b>85% Cotton, 15% Polyester.<br></li>
+                        <li class="list-group-item"><b>Peso de fabrica: </b> 460 GsmFit.<br></li>
+                        <li class="list-group-item"><b>Fit: </b> Baggy.</li>
+                    </ul>
                 </div>
-                <div>
-                    <img src="images/new-colection3.webp" alt="New Colection 3">        
-                    <p><b>Material: </b>85% Cotton, 15% Polyester.<br>
-                        <b>Peso de fabrica: </b> 460 GsmFit.<br>
-                        <b>Fit: </b> Baggy.
-                    </p>
-                </div>  
+            </div>
         </div>
     </div>
-<?php include 'includes/footer.php'; ?>
+</div>
--- index.php
+++ index.php
@@ -8,8 +8,35 @@
         <div class="container-landing">
             <p style="font-family: 'Rubik';">¡AHORA DISPONIBLE</p>
             <p>NUESTRA!</p>
-            <a class="button" href="New_collection.php">NUEVA COLECCIÓN</a>
+            <button id="btnNuevaColeccion" class="btn button">Nueva Colección</button>
+            <div id="seccionColeccion" style="display: none;">
+                <?php include 'New_collection.php'; ?>
+            </div>
         </div>
     </div>
+<script>
 
+    document.getElementById('btnNuevaColeccion').addEventListener('click', function() {
+    var seccion = document.getElementById('seccionColeccion');
+    // Si la sección está oculta, la muestra y luego hace scroll
+
+    if (seccion.style.display === 'none') {
+
+    seccion.style.display = 'block'; // Muestra la sección
+
+    // Desplazamiento suave (scroll)
+
+    seccion.scrollIntoView({ behavior: 'smooth' });
+
+    } else {
+
+    // Si la sección ya está visible, la oculta
+
+    seccion.style.display = 'none';
+
+    }
+
+    });
+
+</script> 
 <?php include 'includes/footer.php'; ?>
